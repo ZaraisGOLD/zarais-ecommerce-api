bceca57e0de5d8abb6c7d13dd190892a33a765ec
ZaraisGOLD <zaraisgold@gmail.com>
2023-06-27 21:28:13 -0400

    test

--- src/tests/test_migrate.js
+++ src/tests/test_migrate.js
@@ -1,8 +1,11 @@
 const sequelize = require('../utils/connection');
+const user = require('./createData/User');
+
 
 const main = async () => {
     try {
         await sequelize.sync({ force: true });
+        await user()
         console.log('Me ejecute');
         process.exit();
     } catch (error) {
